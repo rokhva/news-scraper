ac66378af0c63b519fcceffbf88149f4964b9e6d
Bahaar Rokhva <rokhvab@gmail.com>
2019-11-09 21:12:00 -0700

    added ability to save and delete notes

---
+++ controllers/notes.js
@@ -0,0 +1,36 @@
+let Note = require("../models/note");
+let makeDate = require("../scripts/date");
+
+module.exports = {
+  get: function(data, callback) {
+    Note.find(
+      {
+        _headlineId: data._id
+      },
+      callback
+    );
+  },
+  save: function(data, callback) {
+    let newNote = {
+      _headlineId: data._id,
+      date: makeDate(),
+      noteText: data.noteText
+    };
+    Note.create(newNote, function(err, doc) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(doc);
+        callback(doc);
+      }
+    });
+  },
+  delete: function(data, callback) {
+    Note.remove(
+      {
+        _id: data._id
+      },
+      callback
+    );
+  }
+};
